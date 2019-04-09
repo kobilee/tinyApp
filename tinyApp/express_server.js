dbb70519420151d38fd96329133202b8d216a18b
Kobi Lee <kobiatlaslee@gmail.com>
2019-04-09 17:59:55 +0000

    part 1

--- tinyApp/express_server.js
+++ tinyApp/express_server.js
@@ -1,7 +1,8 @@
 var express = require("express");
 var app = express();
 var PORT = 8080; // default port 8080
-
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
 app.set ("view engine", "ejs");
 
 var urlDatabase = {
@@ -17,6 +18,15 @@ app.get("/urls", (req, res) => {
   let templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+
 app.get("/urls/:shortURL", (req, res) => {
   let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
   res.render("urls_show", templateVars);
