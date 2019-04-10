9d15e09dbcf815e435d2a6dc10166078f460246c
Kobi Lee <kobiatlaslee@gmail.com>
2019-04-10 19:50:55 +0000

    user specific

--- tinyApp/express_server.js
+++ tinyApp/express_server.js
@@ -3,21 +3,17 @@ var cookieParser = require('cookie-parser')
 var app = express();
 var PORT = 8080; // default port 8080
 const bodyParser = require("body-parser");
+const bcrypt = require('bcrypt');
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(cookieParser())
 app.set ("view engine", "ejs");
 
 const users = {};
-
-var urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
-};
+var urlDatabase = {};
 
 //login
 app.post("/login", (req, res) => {
-  console.log(users);
-  console.log(findEmail(req.body.email));
   if (findEmail(req.body.email) && findEmail(req.body.email).password === req.body.password){
     res.cookie("user_id",findEmail(req.body.email));
     res.redirect("/urls");
@@ -35,12 +31,13 @@ app.get("/logout", (req, res) => {
 });
 // register
 app.post("/register", (req, res) => {
+    const password = req.body.password; // found in the req.params object
+    const hashedPassword = bcrypt.hashSync(password, 10);
     if (!findEmail(req.body.email)){
-      console.log(findEmail(req.body.email));
       let r = generateRandomString();
       users[r] = {id : r,
         email: req.body.email,
-        password: req.body.password};
+        password: hashedPassword};
       res.cookie("user_id", users[r]);
       res.redirect("/urls");
     } else {
@@ -51,20 +48,26 @@ app.post("/register", (req, res) => {
 app.post("/urls", (req, res) => {
   let r = generateRandomString();
   if (res.statusCode === 200){
-      urlDatabase[r] = req.body.longURL;  // Log the POST request body to the console
-    }
+      urlDatabase[r] = {};
+      urlDatabase[r].longURL = req.body.longURL;
+      urlDatabase[r].userid = req.cookies["user_id"].id; // Log the POST request body to the console
+  }
   res.redirect("/urls/" + r);         // Respond with 'Ok' (we will replace this)
 });
 
 // delete from database
-app.post("/urls/:shortURL/delete", (req, res) => {
-  delete urlDatabase[req.params.shortURL];
+app.post("/urls/:id/delete", (req, res) => {
+  if(urlDatabase[req.params.id].userid  ===  req.cookies["user_id"].id){
+    delete urlDatabase[req.params.id];
+  }
   res.redirect("/urls");
 });
 
 // update longURL
-app.post("/urls/:shortURL/update", (req, res) => {
-  urlDatabase[req.params.shortURL] = req.body.longURL;
+app.post("/urls/:id", (req, res) => {
+  if(urlDatabase[req.params.id].userid ==  req.cookies["user_id"].id){
+    urlDatabase[req.params.id] = req.body.longURL;
+  }
   res.redirect("/urls");
 });
 
@@ -73,7 +76,8 @@ app.get("/", (req, res) => {
 });
 // links to index
 app.get("/urls", (req, res) => {
-  let templateVars = { urls: urlDatabase , username: req.cookies["user_id"]};
+  let db = filterDB(urlDatabase, req);
+  let templateVars = { urls: db, username: req.cookies["user_id"]};
   res.render("urls_index", templateVars);
 });
 // link to register
@@ -89,13 +93,18 @@ app.get("/login", (req, res) => {
 });
 // rendercreate URL page
 app.get("/urls/new", (req, res) => {
-  let templateVars =  {username: req.cookies["user_id"]}
-  res.render("urls_new", templateVars );
+  if (req.cookies["user_id"] === undefined){
+    res.render("urls_login");
+  }else {
+    let templateVars =  {username: req.cookies["user_id"]}
+    res.render("urls_new", templateVars );
+  }
+
 });
 
 // links to longURL
 app.get("/u/:shortURL", (req, res) => {
-  const longURL = urlDatabase[req.params.shortURL];
+  const longURL = urlDatabase[req.params.shortURL].longURL;
 
   res.redirect(longURL);
 });
@@ -115,16 +124,24 @@ app.listen(PORT, () => {
 });
 
 const findEmail = function(email){
-  console.log("users: ",users);
   for (user of Object.values(users)){
-      console.log("user: ",user);
      if (user.email === email){
         return user;
      }
   }
   return false;
 };
-
+function filterDB(db, req){
+  let new_db = {};
+  if (req.cookies["user_id"]){
+    for (var shortURL in db){
+        if (db[shortURL].userid === req.cookies["user_id"].id){
+          new_db[shortURL] = db[shortURL];
+        }
+    }
+  }
+  return new_db;
+}
 function generateRandomString() {
   return Math.random().toString(36).substring(7);
 }
