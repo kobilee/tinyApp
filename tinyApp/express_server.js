671fa666a7e95664ad36dd6f6c3e9171de17f63c
Kobi Lee <kobiatlaslee@gmail.com>
2019-04-10 16:50:17 +0000

    register form

--- tinyApp/express_server.js
+++ tinyApp/express_server.js
@@ -7,20 +7,37 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(cookieParser())
 app.set ("view engine", "ejs");
 
+const users = {};
+
 var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
 //login
-app.post("/login", (req, res) => {
+app.post("/urls/login", (req, res) => {
   res.cookie("username", req.body.username);
   res.redirect("/urls");
 });
 //logout
-app.post("/logout", (req, res) => {
+app.post("/urls/logout", (req, res) => {
   res.clearCookie("username");
   res.redirect("/urls");
+});
+// register
+app.post("/urls/register", (req, res) => {
+    if (!findEmail(req.body.email)){
+      console.log(findEmail(req.body.email));
+      let r = generateRandomString();
+      res.cookie("user_id", r);
+      users[r] = {id : r,
+        email: req.body.email,
+        password: req.body.password};
+      console.log("users: ",users);
+      res.redirect("/urls");
+    } else {
+      res.status(400).send('email addrees already exists');;
+    }
 });
 //creats new TinyURL
 app.post("/urls", (req, res) => {
@@ -38,8 +55,8 @@ app.post("/urls/:shortURL/delete", (req, res) => {
 });
 
 // update longURL
-app.post("/urls/:id", (req, res) => {
-  urlDatabase[req.params.id] = req.body.longURL;
+app.post("/urls/:shortURL/update", (req, res) => {
+  urlDatabase[req.params.shortURL] = req.body.longURL;
   res.redirect("/urls");
 });
 
@@ -48,13 +65,16 @@ app.get("/", (req, res) => {
 });
 // links to index
 app.get("/urls", (req, res) => {
-  let templateVars = { urls: urlDatabase , username: req.cookies["username"]};
+  let templateVars = { urls: urlDatabase , username: req.cookies["user_id"]};
   res.render("urls_index", templateVars);
 });
-
+// link to register
+app.get("/urls/register", (req, res) => {
+  res.render("urls_register");
+});
 // rendercreate URL page
 app.get("/urls/new", (req, res) => {
-  let templateVars =  {username: req.cookies["username"]}
+  let templateVars =  {username: req.cookies["user_id"]}
   res.render("urls_new", templateVars );
 });
 
@@ -67,7 +87,7 @@ app.get("/u/:shortURL", (req, res) => {
 
 // links to show ShortURL
 app.get("/urls/:shortURL", (req, res) => {
-  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL], username: req.cookies["username"]};
+  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL], username: req.cookies["user_id"]};
   res.render("urls_show", templateVars);
 });
 
@@ -79,6 +99,17 @@ app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
 
+const findEmail = function(email){
+  console.log("users: ",users);
+  for (user of Object.values(users)){
+      console.log("user: ",user);
+     if (user.email === email){
+        return true;
+     }
+
+  }
+  return false;
+};
 
 function generateRandomString() {
   return Math.random().toString(36).substring(7);
